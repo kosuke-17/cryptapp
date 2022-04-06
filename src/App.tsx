deddd65b0d85266a09cd1783d2fb9584cd4f22f6
kosuke-17 <tamutamu17@icloud.com>
2022-04-06 10:32:30 +0900

    画像の変更

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import "./App.css";
 // v6でSwitchがRoutesに変更した
 import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
+import {
+  Navbar,
+  // Homepage,
+  // Exchanges,
+  // Cryptocurrencies,
+  // CryptoDetail,
+  // News,
+} from "./components";
 
 const App = () => {
   return (
@@ -11,8 +18,48 @@ const App = () => {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/">
+                ホームページ
+                {/* <Homepage /> */}
+              </Route>
+              <Route path="/exchanges">
+                両替
+                {/* <Exchanges /> */}
+              </Route>
+              <Route path="/cryptocurrencies">
+                暗号通貨
+                {/* <Cryptocurrencies /> */}
+              </Route>
+              <Route path="/crypto/:coninId">
+                暗号通貨詳細
+                {/* <CryptoDetail /> */}
+              </Route>
+              <Route path="/news">
+                ニュース
+                {/* <News/> */}
+              </Route>
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer">
+        <Typography.Title
+          level={5}
+          style={{ color: "white", textAlign: "center" }}
+        >
+          仮想通貨 <br />
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to="/">ホーム</Link>
+          <Link to="/exchanges">両替</Link>
+          <Link to="/news">ニュース</Link>
+        </Space>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,12 +12,28 @@ const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="logo-container">
-        <Avatar src="img/cryptocurrencieswhite.png" size="default" />
+        {/* ロゴイメージの画像 */}
+        <Avatar src="img/blockchain.png" size="small" />
+        {/* ロゴ用の文字 */}
         <Typography.Title level={2} className="logo">
           <Link to="/">cryptverse</Link>
         </Typography.Title>
-        {/* <Button className="menu-control-container"></Button> */}
       </div>
+      {/* ナビゲーションのアイテム */}
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">ホーム</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">暗号通貨</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">両替</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">ニュース</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 };
